892f002b294bac0681284810f583e91a88d07eed
mcanite <mcanite@byu.edu>
2026-04-02 20:13:51 -0600

    troubleshooting frontend-backend connection

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -12,6 +12,20 @@ export default {
   name: 'SearchView',
   components: {
     SearchBar
+  },
+  data() {
+    return {
+      cars: []
+    }
+  },
+  async mounted() {
+    try {
+      const response = await fetch('/api/cars')
+      const data = await response.json()
+      this.cars = data
+    } catch (error) {
+      console.error('CANNOT FETCH CARS:', error)
+    }
   }
 }
 </script>
