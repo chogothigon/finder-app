7f8da48929f8a5135b2ce7393ca4f699002ee460
mcanite <mcanite@byu.edu>
2026-04-13 10:39:11 -0600

    ryan edit

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -6,7 +6,7 @@
       <div class="team-cards">
         <AboutCard
           name="Ryan"
-          pic="../assets/ryan.jpg"
+          :pic="ryanPic"
           description="Your network security is my priority. My network? Ongoing research project."
           fun="Fun fact"
         />
@@ -41,10 +41,16 @@
 
 <script>
 import AboutCard from '@/components/AboutCard.vue';
+import ryanPic from '@/assets/ryan.jpg';
 
 export default {
   name: 'AboutView',
-  components: {AboutCard}
+  components: {AboutCard},
+  data() {
+    return {
+      ryanPic
+    }
+  }
 }
 </script>
 
